60a904ca4e4069b0e2ae878297c657385c5031e2
Ivan Camilo <rivancamilo@gmail.com>
2020-11-12 22:28:45 -0500

    creamos un insert de prueba y hicimos la conexion con mongoose

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const aplicacion = express();
 const bodyParse = require('body-parser');
-const mongo = require('mongodb');
+const mongoose = require('mongoose');
 
 
 aplicacion.use(bodyParse.json());
 aplicacion.use(bodyParse.urlencoded({ extended: true }));
 
-let pool = mongo.cr
+mongoose.connect('mongodb://localhost:27017/tiendacrud', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 aplicacion.get('/', (req, res) => {
+    const Cat = mongoose.model('Cat', { name: String, apellido: String });
+    const kitty = new Cat({ name: 'Mario Ardilla', apellido: 'Rosales Gomez' });
+    kitty.save().then(() => console.log('meow'));
     res.send(`hola mundo NODEJS`);
 });
 
